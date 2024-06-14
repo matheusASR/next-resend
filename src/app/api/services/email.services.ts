509c82ac0f02345c8037a120794d35c0217c5b82
matheussASR <rm551466@fiap.com>
2024-06-14 11:42:41 -0300

    feat: instalando resend e modificando rota

--- src/app/api/services/email.services.ts
+++ src/app/api/services/email.services.ts
@@ -1,12 +1,35 @@
 /* eslint-disable import/no-anonymous-default-export */
-import { EmailCreate, IEmail } from "../interfaces/index.ts";
+import { IEmail } from "../interfaces/index.ts";
 import { emailRepository } from "../repositories/index.ts";
 import { emailSchema } from "../schemas/index.ts";
 import { DeepPartial } from "typeorm";
 import { Email } from "../entities/index.ts";
+import { Resend } from "resend";
+import 'dotenv/config';
 
 const create = async (payload: any): Promise<IEmail> => {
-  const emailCreated: any = emailRepository.create(payload);
+  const receivers = payload.receivers
+  const emails_receivers = receivers.map((receiver: any) => receiver.email);
+
+  const resend = new Resend(process.env.RESEND_API_KEY)
+
+  if (payload.sender.length === 0) {
+    payload.sender = "rstcom@rstcom.com.br"
+  }
+
+  const { data, error } = await resend.emails.send({
+    from: `Acme <onboarding@resend.dev>`,
+    to: emails_receivers,
+    subject: payload.subject,
+    html: "<strong>it works!</strong>",
+  });
+
+  if (error) {
+    console.log(error);
+  }
+  console.log(data)
+  
+  const emailCreated = emailRepository.create(payload);
   await emailRepository.save(emailCreated);
 
   return emailSchema.parse(emailCreated);
